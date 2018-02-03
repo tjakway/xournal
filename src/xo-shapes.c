5bda447cfa3966c9c439a6aaa774ffe327f3918b
Thomas Jakway <tjakway@nyu.edu>
2018-02-03 14:55:09 -0800

    removed inline traits to fix build problems

--- src/xo-shapes.c
+++ src/xo-shapes.c
@@ -73,42 +73,42 @@ void calc_inertia(double *pt, int start, int end, struct Inertia *s)
 
 /* compute normalized quantities */
 
-inline double center_x(struct Inertia s) 
+double center_x(struct Inertia s) 
 { 
   return s.sx/s.mass;
 }
 
-inline double center_y(struct Inertia s) 
+double center_y(struct Inertia s) 
 { 
   return s.sy/s.mass;
 }
 
-inline double I_xx(struct Inertia s)
+double I_xx(struct Inertia s)
 {
   if (s.mass <= 0.) return 0.;
   return (s.sxx - s.sx*s.sx/s.mass)/s.mass;
 }
 
-inline double I_xy(struct Inertia s)
+double I_xy(struct Inertia s)
 {
   if (s.mass <= 0.) return 0.;
   return (s.sxy - s.sx*s.sy/s.mass)/s.mass;
 }
 
-inline double I_yy(struct Inertia s)
+double I_yy(struct Inertia s)
 {
   if (s.mass <= 0.) return 0.;
   return (s.syy - s.sy*s.sy/s.mass)/s.mass;
 }
 
-inline double I_rad(struct Inertia s)
+double I_rad(struct Inertia s)
 {
   double ixx = I_xx(s), iyy = I_yy(s);
   if (ixx+iyy <= 0.) return 0.;
   return sqrt(ixx+iyy);
 }
 
-inline double I_det(struct Inertia s)
+double I_det(struct Inertia s)
 {
   double ixx = I_xx(s), iyy = I_yy(s), ixy = I_xy(s);
   if (s.mass <= 0.) return 0.;
