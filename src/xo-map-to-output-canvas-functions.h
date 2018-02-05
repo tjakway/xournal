95a47fae2de087f1902405094265f04ba1a4b1fb
Thomas Jakway <tjakway@nyu.edu>
2018-02-04 17:46:25 -0800

    added prototype for map_to_output_get_canvas_drawing_area_dimensions

--- src/xo-map-to-output-canvas-functions.h
+++ src/xo-map-to-output-canvas-functions.h
@@ -3,6 +3,8 @@
 #include <glib.h>
 #include <libgnomecanvas/libgnomecanvas.h>
 
+#include "xournal.h"
+
 #include "xo-map-to-output-error.h"
 #include "xo-map-to-output.h"
 
@@ -17,3 +19,10 @@ gboolean map_to_output_mapping_is_correct(MapToOutput*, MapToOutputError*);
 
 //map_to_output_mapped_rect_is_visible should return true after calling this
 void map_to_output_scroll_to_mapped_rect(MapToOutput*, MapToOutputError*);
+
+/**
+ * returns the top left x, y, width and height of the drawing area of the canvas widget
+ * (i.e. excluding the non-drawable background area)
+ */
+void map_to_output_get_canvas_drawing_area_dimensions(
+        GnomeCanvas*, Page*, gint*, gint*, double*, double*, MapToOutputError*);
