708acb77dcb56e377e6dfeb7f7eb2db33af3551f
Thomas Jakway <tjakway@nyu.edu>
2018-02-06 18:01:16 -0800

    wrote prints for w2c and the affine matrix

--- src/xo-debug.c
+++ src/xo-debug.c
@@ -4,6 +4,9 @@
 
 #include <stdio.h>
 #include <glib.h>
+#include <float.h> //for DBL_MAX
+
+#include <libgnomecanvas/libgnomecanvas.h>
 
 #ifdef DEBUG
 
@@ -11,19 +14,21 @@
 //info when stopped at breakpoints
 
 
+//return if any necessary globals are null
+#define DEBUG_PRINT_CHECK_GLOBALS() do { if(canvas == NULL) \
+    { \
+        printf("%s: can't print because canvas == NULL\n", __func__); \
+        return; \
+    } \
+    if(ui.cur_page == NULL) \
+    { \
+        printf("%s: can't print because ui.cur_page == NULL\n", __func__); \
+        return; \
+    } } while(0);
+
 void print_canvas_drawing_area_dimensions()
 {
-    //print info from globals
-    if(canvas == NULL)
-    {
-        printf("%s: can't print because canvas == NULL\n", __func__);
-        return;
-    }
-    if(ui.cur_page == NULL)
-    {
-        printf("%s: can't print because ui.cur_page == NULL\n", __func__);
-        return;
-    }
+    DEBUG_PRINT_CHECK_GLOBALS();
 
     Page* page = ui.cur_page;
 
@@ -58,5 +63,54 @@ void print_canvas_drawing_area_dimensions()
     }
 }
 
+void print_canvas_w2c_affine_matrix()
+{
+    DEBUG_PRINT_CHECK_GLOBALS();
+
+    const int affine_len = 6;
+    double affine[affine_len];
+
+    for(int i = 0; i < affine_len; i++)
+    {
+        affine[i] = DBL_MAX;
+    }
+
+    gnome_canvas_w2c_affine(canvas, affine);
+
+    gboolean affine_not_set = FALSE;
+    for(int i = 0; i < affine_len; i++)
+    {
+        if(affine[i] == DBL_MAX)
+        {
+            affine_not_set = TRUE;
+        }
+    }
+
+    if(affine_not_set)
+    {
+        fprintf(stderr, "gnome_canvas_w2c_affine didn't set the affine matrix in %s!\n", __func__);
+    }
+    else
+    {
+        printf("w2c affine matrix: { %f, %f, %f, %f, %f, %f }\n",
+                affine[0], affine[1], affine[2], affine[3], affine[4], affine[5]);
+    }
+}
+
+void print_canvas_w2c(double x, double y)
+{
+    DEBUG_PRINT_CHECK_GLOBALS();
+
+    int ret_x = -1, ret_y = -1;
+
+    gnome_canvas_w2c_d(canvas, x, y, &ret_x, &ret_y);
+
+    if(ret_x < 0 || ret_y < 0)
+    {
+        fprintf(stderr, "WARNING: gnome_canvas_w2c_d returned x < 0 || y < 0 in %s\n", __func__);
+    }
+
+    printf("print_canvas_w2c(%f, %f): (%d, %d)\n", x, y, ret_x, ret_y);
+}
 
 #endif
